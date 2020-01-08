4636948e0ea432f04cbf34e0ad6aa1891ac06a47
pro-programmer-coder <lawrencej.london2019a@gmail.com>
2020-01-08 11:04:07 +0000

    commit 1

--- src/main/java/core/NumberCruncher.java
+++ src/main/java/core/NumberCruncher.java
@@ -4,8 +4,16 @@ import java.util.Arrays;
 
 public class NumberCruncher {
     public int highestIntArray(int[] input){
-//        return Arrays.stream(array).max().orElseThrow();
-        return input[0];
+        if(input.length == 1){
+            return input[0];
+        }
+        if(input[0] > input[1]){
+            return input[0];
+        }
+        else{
+            return input[1];
+        }
 
+        Array.stram
     }
 }
--- src/test/java/core/NumberCruncherTest.java
+++ src/test/java/core/NumberCruncherTest.java
@@ -22,4 +22,64 @@ class NumberCruncherTest {
         assertEquals(expected,actual);
     }
 
+    @Test
+    public void two_int_sort_biggest_first(){
+        //arrange
+        int expected = 13;
+        int actual;
+        int[] input = new int[]{13, 4};
+        NumberCruncher cut = new NumberCruncher();
+
+        //act
+        actual = cut.highestIntArray(input);
+
+        //assert
+        assertEquals(expected,actual);
+    }
+
+    @Test
+    public void two_int_sort_biggest_not_first(){
+        //arrange
+        int expected = 13;
+        int actual;
+        int[] input = new int[]{7,13};
+        NumberCruncher cut = new NumberCruncher();
+
+        //act
+        actual = cut.highestIntArray(input);
+
+        //assert
+        assertEquals(expected,actual);
+    }
+
+    @Test
+    public void same_value_twice(){
+        //arrange
+        int expected = 13;
+        int actual;
+        int[] input = new int[]{13,13};
+        NumberCruncher cut = new NumberCruncher();
+
+        //act
+        actual = cut.highestIntArray(input);
+
+        //assert
+        assertEquals(expected,actual);
+    }
+
+    @Test
+    public void biggest_int_sort(){
+        //arrange
+        int expected = 11;
+        int actual;
+        int[] input = new int[]{4,5,-8,3,11,-21,6};
+        NumberCruncher cut = new NumberCruncher();
+
+        //act
+        actual = cut.highestIntArray(input);
+
+        //assert
+        assertEquals(expected,actual);
+    }
+
 }
