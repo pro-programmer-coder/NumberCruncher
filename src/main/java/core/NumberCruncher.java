b9605799add24145ce41e8f4e1557d7789eb8ccb
pro-programmer-coder <lawrencej.london2019a@gmail.com>
2020-01-08 11:06:27 +0000

    commit 2

--- src/main/java/core/NumberCruncher.java
+++ src/main/java/core/NumberCruncher.java
@@ -4,16 +4,16 @@ import java.util.Arrays;
 
 public class NumberCruncher {
     public int highestIntArray(int[] input){
-        if(input.length == 1){
-            return input[0];
-        }
-        if(input[0] > input[1]){
-            return input[0];
-        }
-        else{
-            return input[1];
-        }
+//        if(input.length == 1){
+//            return input[0];
+//        }
+//        if(input[0] > input[1]){
+//            return input[0];
+//        }
+//        else{
+//            return input[1];
+//        }
 
-        Array.stram
+        return Arrays.stream(input).max().orElse(0);
     }
 }
